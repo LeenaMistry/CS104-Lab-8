58fe6dfe029b4e420d5472c10eea3ff9948cea09
Leena Mistry <leenahmistry@gmail.com>
2024-11-14 11:27:04 -0500

    lab complete

---
+++ lab.js
@@ -0,0 +1,167 @@
+
+//Question 1
+var firstname;
+var firstname = "Leena";
+console.log(firstname);
+var age;
+var age = "18";
+console.log(age);
+
+//Question 2
+var numbers;
+var numbers= "88";
+console.log(numbers);
+
+//Question 3
+let Question3;
+
+let RollNumber = 15;
+if (RollNumber > 10){
+    Question3 = "Roll Number is greater than 10";
+    console.log(Question3);
+}
+
+//Question 4
+var isMember;
+var isMember = true
+
+if (isMember = "true"){
+    isMember = "Member discount applied";
+    console.log(isMember);
+}
+
+//Question 5
+let Value;
+let startValue = 50;
+if (startValue > 0){
+    Value = "positive";
+    console.log(Value);
+
+}
+else if (startValue < 0){
+    Value = "negative";
+    console.log(Value);
+}
+else if (startValue = 0){
+    Value = "zero";
+    console.log(Value);
+}
+else{
+    startValue = "Fail";
+    console.log(Value) ;
+}
+
+//Question 6
+let a = 8
+let b = 18
+console.log(a >= b)
+
+//let a = 8
+//let b = 18
+//console.log(a == b)
+//false
+
+//let a = 8
+//let b = 18
+//console.log(a === b)
+//false
+
+//let a = 8
+//let b = 18
+//console.log(a != b)
+//true
+
+//let a = 8
+//let b = 18
+//console.log(a < b)
+//true
+
+//let a = 8
+//let b = 18
+//console.log(a > b)
+//false
+
+//let a = 8
+//let b = 18
+//console.log(a <= b)
+//true
+
+//let a = 8
+//let b = 18
+//console.log(a >= b)
+//false
+
+
+//Question 7
+var FirstName;
+var FirstName = "Leena";
+
+var LastName;
+var LastName = "Mistry";
+
+var Age;
+var Age = "18";
+
+console.log("Hello", FirstName, LastName, "you are", Age, "years old")
+
+//Question 8
+let numb;
+var num = 50;
+if(num > 0 && num < 100){ 
+    numb = "the number is within range";
+    console.log(numb);
+}
+
+//Question 9
+var drive;
+
+var hasCar;
+var hasCar = "true";
+
+var hasLicense;
+var hasLicense = "true";
+
+var hasInsurance;
+var hasInsurance = "true";
+
+if(hasCar && hasLicense && hasInsurance){
+    drive = "you can drive leaglly";
+    console.log(drive)
+}
+else{ drive = "check your driving eligibility";
+    console.log(drive)
+}
+
+//Question 10
+var numbe;
+
+var num = 21
+if(num %2 == 0){
+    console.log("the number is even")
+}
+else{
+    console.log("the number is odd")
+}
+
+//Question 11
+let grade;
+
+let score = "85";
+if (score > 90 ){
+    grade = "Grade A";
+    console.log(grade);
+
+}
+else if (score > 80 ){
+    grade = "Grade B";
+    console.log(grade);
+}
+
+else if (score > 89) {
+    grade = "Grade C";
+    console.log(grade) ;
+}
+
+
+
+
